f82c22d4cc76ff918d2fb3b8665b2dbc569c3e61
deokhwa_lee <deokhwa_lee@tmax.co.kr>
2021-09-09 12:34:21 +0900

    Add timer function

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -4,17 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var timerTask: Timer? = null
+        var sec: Int = 0
         val tv: TextView = findViewById(R.id.tv_hello)
         val btn: Button = findViewById(R.id.btn_kor)
+        var isRunning = false
 
         btn.setOnClickListener {
-            tv.text = "안녕"
+            isRunning = !isRunning
+            if (isRunning) {
+                timerTask = kotlin.concurrent.timer(period = 1000) {
+                    sec++
+                    runOnUiThread() {
+                        tv.text = sec.toString()
+                    }
+                }
+            }
+            else {
+                timerTask?.cancel()
+            }
         }
+
+
     }
 }
